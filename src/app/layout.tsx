f415ba377c2296916f17a13117cf85f7d2042170
sahalhes <sahaldevelop@gmail.com>
2025-06-21 14:28:37 +0530

    k

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "YtAud",
   description: "Youtube Looper",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
